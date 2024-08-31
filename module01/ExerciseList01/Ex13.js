9847255aeb8f1be1edbf8973250ab1b6f7cad72d
ronaldosnavarro <navarro.silva@terra.com.br>
2024-08-31 19:19:55 -0300

    Add Ex13

---
+++ module01/ExerciseList01/Ex13.js
@@ -0,0 +1,19 @@
+/*
+13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer
+a média aritmética desses números.
+*/
+
+const prompt = require('prompt-sync')();
+
+let somaNumeros = 0;
+let contador = 0;
+let numeroDecimal = 1; // Exemplo de número fornecido
+
+while (numeroDecimal !== 0) {
+    numeroDecimal = parseFloat(prompt("Digite um número decimal | 0 Para sair: "));
+    somaNumeros += numeroDecimal;
+    contador++;
+}
+
+let media = somaNumeros / contador;
+console.log(`A média aritmética é: ${media}`);
