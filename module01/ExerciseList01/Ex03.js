04c7f1b224647aa9e4ae1c36ac4a06bd33027b00
ronaldosnavarro <navarro.silva@terra.com.br>
2024-08-31 17:01:11 -0300

    Add Ex03

---
+++ module01/ExerciseList01/Ex03.js
@@ -0,0 +1,16 @@
+/*
+Implemente um programa que recebe uma nota de 0 a 10 e classifica como
+"Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+*/
+
+const prompt = require('prompt-sync')()
+
+let nota = prompt("Digite a nota: ")
+
+if (nota >= 7) {
+    console.log("Aprovado");
+} else if (nota >= 5 && nota < 7) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
