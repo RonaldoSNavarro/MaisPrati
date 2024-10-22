314153708f9c41487d748724f15bdb6f7c8d4dbe
ronaldosnavarro <navarro.silva@terra.com.br>
2024-10-21 22:28:58 -0300

    aula 38 continuação - rest-spread

--- 2-html-css-js/5-js-intermediate/4-rest-spread.js
+++ 2-html-css-js/5-js-intermediate/4-rest-spread.js
@@ -1,12 +1,40 @@
-let numeros = [1, 2, 3]
-let novosNumeros = [...numeros, 4, 5]
+// let numeros = [1, 2, 3]
+// let novosNumeros = [...numeros, 4, 5]
 
-console.log(novosNumeros)
+// console.log(novosNumeros)
 
 
-// spread com objetos
+// // spread com objetos
 
-let pessoa = {nome: "Ronaldo", idade: 37}
-let novaPessoa = {...pessoa, cidade: 'Belém'}
+// let pessoa = {nome: "Ronaldo", idade: 37}
+// let novaPessoa = {...pessoa, cidade: 'Belém'}
 
-console.log(novaPessoa)
+// console.log(novaPessoa)
+
+// function soma(a, b, c) {
+//     return a + b + c
+// }
+
+// let numeros = [1, 2, 3]
+// console.log(soma(...numeros))
+
+
+// rest
+
+function soma (...numeros) {
+    return numeros.reduce((total, numero) => total + numero)
+}
+
+console.log(soma(1, 2, 3, 4, 5))
+
+const [primeiro, segundo, ...resto] = [1, 2, 3, 4, 5]
+
+console.log(primeiro)
+console.log(segundo)
+console.log(resto)
+
+const pessoa = {nome: 'Slash', idade: 60, banda: 'Guns n Roses'}
+const {nome, ...outrasInfo} = pessoa
+
+console.log(nome)
+console.log(outrasInfo)
