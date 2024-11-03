6bddfd2d45d4482ff4fefaa33a30300becb0a4a8
ronaldosnavarro <navarro.silva@terra.com.br>
2024-11-02 22:05:29 -0300

    Aula 45 - Componentes - Estado, Ciclo de vida e Props

---
+++ 3-reactjs/3-components/src/components/LifeCycleFunctionalComponents.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function LifeCycleFunctionComponent (){
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log("Componente montado")
+        return() => {console.log("Componente será desmontado")}
+    }, [])
+
+    useEffect(() => {
+        console.log("Componente atualizado!")
+    }, [count])
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <p>Contagem: {count}</p>
+        
+        <button onClick={increment}>Incrementar a contagem</button>
+        </div>
+    )
+}
+
+export default LifeCycleFunctionComponent
