ee8548e4aa07e5d96d84d46a9bb0a9ecc5d7440a
ronaldosnavarro <navarro.silva@terra.com.br>
2024-11-07 18:24:00 -0300

    Aula 46 - Exercícios / Eventos e manipulação de dados

--- 3-reactjs/4-events-data/form/src/components/form.jsx
+++ 3-reactjs/4-events-data/form/src/components/form.jsx
@@ -13,12 +13,27 @@ function Form() {
     if (novaTarefa.trim() === "") {
       return;
     }
-    setTarefas([...tarefas, novaTarefa]);
+    setTarefas([...tarefas, { texto: novaTarefa, concluida: false }]);
     setNovaTarefa("");
   };
 
   const handleRemoveTask = (index) => {
     const updatedTasks = tarefas.filter((_, i) => i != index);
+    setTarefas(updatedTasks);
+  };
+
+  const handleCompletedTask = (index) => {
+    const updatedTasks = tarefas.map((tarefa, i) => {
+      if (i === index) {
+        return { ...tarefa, concluida: !tarefa.concluida };
+      }
+      return tarefa;
+    });
+    setTarefas(updatedTasks);
+  };
+
+  const handleClearCompletedTask = () => {
+    const updatedTasks = tarefas.filter((tarefa) => !tarefa.concluida);
     setTarefas(updatedTasks);
   };
 
@@ -35,14 +50,34 @@ function Form() {
 
       <ul>
         {tarefas.map((tarefa, index) => (
-          <li key={index}>
-            {tarefa}
+          <li
+            key={index}
+            style={{
+              textDecoration: tarefa.concluida ? "line-through" : "none",
+            }}
+          >
+            {tarefa.texto}
+            <button onClick={() => handleCompletedTask(index)}>
+              {tarefa.concluida ? "Desfazer" : "Concluído"}
+            </button>
             <button onClick={() => handleRemoveTask(index)}>Remover</button>
           </li>
         ))}
       </ul>
+
+      <button onClick={handleClearCompletedTask}>Limpar Tarefas Concluídas</button>
     </div>
   );
 }
 
 export default Form;
+
+// Exercício
+// Marcar tarefa como concluída:
+
+// Ao lado de cada tarefa, inclua um botão "Concluído". Quando clicado, o texto da tarefa deve ser riscado, indicando que a tarefa foi concluída.
+// Utilize um estilo de texto riscado (por exemplo, text-decoration: line-trough;) para representar a conclusão.
+
+// Limpar tarefas concluídas:
+
+// Adicione um botão "Limpar tarefas concluídas" abaixo da lista de tarefas. Esse botão deve remover todas as tarefas que foram marcadas como concluídas.
