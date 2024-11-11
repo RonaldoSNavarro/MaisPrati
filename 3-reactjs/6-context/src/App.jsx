09aa27be5903d4fc2102e23ecb5faef0f9a94630
ronaldosnavarro <navarro.silva@terra.com.br>
2024-11-10 23:14:46 -0300

    Aula 48 - Genrenciamento de Estado com Context API

---
+++ 3-reactjs/6-context/src/App.jsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "../components/ThemeContext";
+import ThemeSwitcher from "../components/ThemeSwitcher";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <h1>Exemplo de tema com useContext</h1>
+        <ThemeSwitcher />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
