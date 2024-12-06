702c2f89f1c598d11289f349a9eab45a844feba0
ronaldosnavarro <navarro.silva@terra.com.br>
2024-12-06 15:10:14 -0300

    Refatoramento ex waitingLine

--- 4-java/structures/src/ds/WaitingLine.java
+++ 4-java/structures/src/ds/WaitingLine.java
@@ -1,14 +1,5 @@
 package ds;
 
-//Simulando uma Fila de Espera:
-
-//Crie um programa que simule uma fila de espera utilizando uma LinkedList.
-//Adicione os seguintes nomes na fila: "Ana", "Pedro", "Maria", "João".
-//    Mostre o próximo nome a ser atendido (primeiro da fila) e remova-o.
-//      Permita que o usuário insira novos nomes na fila ou remova o próximo nome a ser atendido até que a fila esteja vazia.
-//Mostre a fila a cada operação.
-
-
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -21,8 +12,13 @@ public class WaitingLine {
         linkedListQueue.add("Maria");
         linkedListQueue.add("João");
 
-        System.out.println("Próximo a ser atendido: " + linkedListQueue.peek());
+        System.out.println("Próximo(a) a ser atendido(a) --> " + linkedListQueue.peek());
         linkedListQueue.remove(linkedListQueue.peek());
+        System.out.print("""
+                --------------------------------------------------------------
+                                      Fila atualizada
+                --------------------------------------------------------------
+                """);
         linkedListQueue.forEach(System.out::println);
 
         Scanner scanner = new Scanner(System.in);
@@ -31,7 +27,11 @@ public class WaitingLine {
 
         while (op != 3) {
 
-            System.out.println("""
+            if (linkedListQueue.isEmpty()) {
+                System.out.println("A fila está vazia!");
+                break;
+            } else {
+                System.out.println("""
                     --------------------------------------------------------------
                     Deseja inserir novos nomes ou remover o próximo a ser atendido?
                     1 - Adicionar
@@ -39,35 +39,31 @@ public class WaitingLine {
                     3 - Sair
                     --------------------------------------------------------------
                     """);
-            op = scanner.nextInt();
-            switch (op) {
-                case 1:
-                    System.out.print("Digite o nome: ");
-                    scanner.nextLine();
-                    nome = scanner.nextLine();
-                    linkedListQueue.add(nome);
-                    System.out.println("--- Adicionado com sucesso! ---");
-                    linkedListQueue.forEach(System.out::println);
-                    break;
-
-                case 2:
-                    if (linkedListQueue.isEmpty()) {
-                        System.out.println("A fila está vazia!");
+                op = scanner.nextInt();
+                switch (op) {
+                    case 1:
+                        System.out.print("Digite o nome: ");
+                        scanner.nextLine();
+                        nome = scanner.nextLine();
+                        linkedListQueue.add(nome);
+                        System.out.println("--- Adicionado com sucesso! ---");
+                        linkedListQueue.forEach(System.out::println);
                         break;
-                    } else {
+
+                    case 2:
+
                         linkedListQueue.remove(linkedListQueue.peek());
                         System.out.println("--- Removido com sucesso! ---");
                         linkedListQueue.forEach(System.out::println);
                         break;
-                    }
-                case 3:
-                    break;
 
-                default:
-                    System.out.println("Opção inválida.");
+                    case 3:
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida.");
+                }
             }
         }
-
-
     }
 }
