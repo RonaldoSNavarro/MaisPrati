5d77105da63544db6b512fcd40a8a7f1f5203f64
ronaldosnavarro <navarro.silva@terra.com.br>
2024-12-09 15:15:06 -0300

    Aula 64 - Threads

--- 4-java/structures/src/ds/JavaCollections.java
+++ 4-java/structures/src/ds/JavaCollections.java
@@ -2,7 +2,7 @@ package ds;
 
 import java.util.*;
 
-public class App {
+public class JavaCollections {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("Java");
